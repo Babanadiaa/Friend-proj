6d479db570601094433b331636b540656324fb43
Maksym Atamanchuk <maksumatmn@gmail.com>
2025-10-09 15:07:51 +0300

    update project files

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/Friend-proj/', // 👈 обов’язково вказуєш назву репозиторію
+  build: {
+    sourcemap: false, // вимикає джерельні карти, де з’являється eval()
+  },
+  esbuild: {
+    legalComments: 'none', // прибирає коментарі типу eval з білду
+  },
 })
